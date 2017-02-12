29da19960be2a325e111d363bdc2226a2294be6b
Mark Reichert <mark.a.reichert1@gmail.com>
2017-02-12 16:23:50 -0600

    first primitive page

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ import jinja2
 
 from google.appengine.ext import db
 
-
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
@@ -35,32 +34,33 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Art(db.Model):
+class Blog(db.Model):
     title = db.StringProperty(required = True)
-    art = db.TextProperty(required = True)
+    blog = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
-    def render_front(self, title="", art="", error=""):
-        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+    def render_front(self, title="", blog="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
 
-        self.render("front.html", title=title, art=art, error=error, arts=arts)
+        self.render("front.html", title=title, blog=blog, error=error, blogs=blogs)
 
     def get(self):
         self.render_front()
 
     def post(self):
         title = self.request.get("title")
-        art = self.request.get("art")
+        blog = self.request.get("blog")
 
-        if title and art:
-            a = Art(title = title, art = art)
-            a.put()
+        if title and blog:
+            b = Blog(title = title, blog = blog)
+            b.put()
 
             self.redirect("/")
+            return
         else:
-            error = "we need both a title and some artwork"
-            self.render_front(title, art, error)
+            error = "we need both a title and some blog text"
+            self.render_front(title, blog, error)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
